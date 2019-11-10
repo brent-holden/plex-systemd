bcc1b5596264dcf6c40b35dd4a7185b4085a4070
Brent Holden <brent.holden@gmail.com>
2019-11-10 02:30:02 -0500

    Now points to using podman instead of docker

--- scripts/update_container_images.sh
+++ scripts/update_container_images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-for i in $(docker images -n | awk '{print $1":"$2}'); do
-    docker pull $i -q
+for i in $(podman images -n | awk '{print $1":"$2}'); do
+    podman pull $i -q
 done
