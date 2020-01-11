2e33b167b4fe93c582f27b994f216d4bab2b92ed
Brent Holden <brent.holden@gmail.com>
2020-01-11 18:33:17 -0500

    Updated setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,9 +18,15 @@ source ${BASH_SOURCE%/*}/setup_backup.sh
 read -r -p "Would you like to restore service configs from backups? [y/N] " RESPONSE
 RESPONSE=${RESPONSE,,}
 if [[ "$RESPONSE" =~ ^(yes|y)$ ]]
+then
   source ${BASH_SOURCE%/*}/restore_services.sh
+else
+  echo "Skipping restoration process"
 fi
 
-echo -e "\n\n"
-echo "You should now be able to reach all of your services at https://<hostname> using /lidarr, /radarr, /sonarr, etc."
-echo "Done."
+echo -e "\n"
+echo "You should now be able to reach Ombi at https://<hostname>"
+echo "Plex will be available at http://<hostname>:32400/web/"
+echo "Services will be available at https://<hostname>/{radarr,sonarr,lidarr,tautulli,hydra2,sabnzbd}"
+echo -e "\n"
+echo "Done with installation. Exiting"
