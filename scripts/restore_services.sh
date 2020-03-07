c2276ea48a6e51a34453642817ed17a38eebc332
Brent Holden <brent.holden@gmail.com>
2020-03-07 15:22:58 -0500

    Removed service startup after restoration

--- scripts/restore_services.sh
+++ scripts/restore_services.sh
@@ -21,32 +21,32 @@ cd $TMPDIR
 # Loop over services defined
 for SERVICE in "${SERVICES[@]}"; do
 
-# Set some variables
-LATEST=backup_$BACKUPDATE.tar.gz
-SRCDIR=$RCLONEBACKUPDIR/$SERVICE
-DESTDIR=$OPTDIR/$SERVICE
+  # Set some variables
+  LATEST=backup_$BACKUPDATE.tar.gz
+  SRCDIR=$RCLONEBACKUPDIR/$SERVICE
+  DESTDIR=$OPTDIR/$SERVICE
 
-# Stop service before config restoration
-echo "Stopping $SERVICE"
-sudo systemctl stop $SERVICE
+  # Stop service before config restoration
+  echo "Stopping $SERVICE"
+  sudo systemctl stop $SERVICE
 
-# Copy the backup over locally
-echo "Copying over backup $SRCDIR/$LATEST to $TMPDIR"
-sudo rsync --info=progress $SRCDIR/$LATEST $TMPDIR
-#cp $SRCDIR/$LATEST $TMPDIR
+  # Copy the backup over locally
+  echo "Copying over backup $SRCDIR/$LATEST to $TMPDIR"
+  sudo rsync --info=progress $SRCDIR/$LATEST $TMPDIR
+  #cp $SRCDIR/$LATEST $TMPDIR
 
-# Extract the backup file
-echo "Extracting backup to $DESTDIR"
-sudo tar -zxf $TMPDIR/$LATEST --directory $DESTDIR
-echo "Done extracting files"
+  # Extract the backup file
+  echo "Extracting backup to $DESTDIR"
+  sudo tar -zxf $TMPDIR/$LATEST --directory $DESTDIR
+  echo "Done extracting files"
 
-# Change directory ownership
-echo "Changing directory ownership of $DESTDIR to $PLEXUSER.$PLEXGROUP"
-sudo chown -R $PLEXUSER.$PLEXUSER $DESTDIR
+  # Change directory ownership
+  echo "Changing directory ownership of $DESTDIR to $PLEXUSER.$PLEXGROUP"
+  sudo chown -R $PLEXUSER.$PLEXUSER $DESTDIR
 
-# Start service
-echo "Starting $SERVICE"
-sudo systemctl start $SERVICE
+  # Start service
+  #echo "Starting $SERVICE"
+  #sudo systemctl start $SERVICE
 
 done
 
