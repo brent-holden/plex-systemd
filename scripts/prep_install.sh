9d72d550be478646d53f2dbede637edad5492bd5
Brent Holden <brent.holden@gmail.com>
2020-03-07 15:13:26 -0500

    Added cockpit enable

--- scripts/prep_install.sh
+++ scripts/prep_install.sh
@@ -12,24 +12,30 @@ sudo adduser $PLEXUSER --uid=$PLEXUID -U
 echo "Installing packages: $PACKAGES"
 sudo yum -y install $PACKAGES
 
+# Enable cockpit
+echo "Enabling cockpiti for remote web administration. Access via https://<ip>:9090"
+sudo systemctl enable --now cockpit.socket
+
 # Set default zone to trusted assuming you're on a private net behind a firewall
 echo "Setting FirewallD to live in a trusted zone. Comment this out if you're in a DMZ"
 sudo firewall-cmd --set-default-zone=trusted
 
-# Disable SELinux because reasons
+# Disable SELinux because reasons. Sorry Dan
 echo "Disabling SELinux. This will require a reboot to take effect"
 sudo sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/sysconfig/selinux
 
 # Create download directories
 for MEDIATYPE in "${DOWNLOADABLES[@]}"; do
 
-MEDIADIR=$COMPLETEDDIR/$MEDIATYPE
+  MEDIADIR=$COMPLETEDDIR/$MEDIATYPE
 
-echo "Creating $MEDIADIR"
-sudo mkdir -p $MEDIADIR
+  echo "Creating $MEDIADIR"
+  sudo mkdir -p $MEDIADIR
 
-echo "Changing permissions on $MEDIADIR to $PLEXUSER.$PLEXGROUP"
-sudo chown -R $PLEXUSER.$PLEXGROUP $MEDIADIR
+  echo "Changing permissions on $MEDIADIR to $PLEXUSER.$PLEXGROUP"
+  sudo chown -R $PLEXUSER.$PLEXGROUP $MEDIADIR
+
+done
 
 echo "Creating $TRANSCODEDIR"
 sudo mkdir -p $TRANSCODEDIR
@@ -37,5 +43,4 @@ sudo mkdir -p $TRANSCODEDIR
 echo "Changing permissions on $TRANSCODEDIR to $PLEXUSER.$PLEXGROUP"
 sudo chown -R $PLEXUSER.$PLEXGROUP $TRANSCODEDIR
 
-done
-
+echo "Done preparing your system. Ready for services installation"
