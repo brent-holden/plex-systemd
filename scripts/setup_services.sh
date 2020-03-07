71900225ea6f9b79501e92c1674c0b061e29a333
Brent Holden <brent.holden@gmail.com>
2020-03-07 15:22:26 -0500

    Removed service startup and cleanups

--- scripts/setup_services.sh
+++ scripts/setup_services.sh
@@ -6,31 +6,30 @@ echo -e "\n\n### Setting up Services ###\n\n"
 
 # Loop over services defined
 for SERVICE in "${SERVICES[@]}"; do
-SVCDIR=$OPTDIR/$SERVICE
+  SVCDIR=$OPTDIR/$SERVICE
 
-echo -e "\nSetting up ${SERVICE}"
+  echo -e "\nSetting up ${SERVICE}"
 
-# Create the service directory
-echo "Creating service directory: $SVCDIR"
-sudo mkdir -p $SVCDIR
+  # Create the service directory
+  echo "Creating service directory: $SVCDIR"
+  sudo mkdir -p $SVCDIR
 
-# Change directory ownership
-echo "Changing ownership to $PLEXUSER.$PLEXGROUP"
-sudo chown -R $PLEXUSER.$PLEXGROUP $SVCDIR
+  # Change directory ownership
+  echo "Changing ownership to $PLEXUSER.$PLEXGROUP"
+  sudo chown -R $PLEXUSER.$PLEXGROUP $SVCDIR
 
-# Copy service file over
-echo "Copying $SERVICE.service to $SYSTEMDDIR"
-sudo cp ${BASH_SOURCE%/*}/$SYSTEMDSVCFILESDIR/$SERVICE.service $SYSTEMDDIR
+  # Copy service file over
+  echo "Copying $SERVICE.service to $SYSTEMDDIR"
+  sudo cp ${BASH_SOURCE%/*}/$SYSTEMDSVCFILESDIR/$SERVICE.service $SYSTEMDDIR
 
-# Reload systemd
-echo "Reloading systemd"
-sudo systemctl daemon-reload
+  # Reload systemd
+  echo "Reloading systemd"
+  sudo systemctl daemon-reload
 
-# Enable and start services
-echo -e "\n"
-echo "Enabling and starting $SERVICE"
-sudo systemctl enable $SERVICE
-#sudo systemctl start $SERVICE
+  # Enable
+  echo -e "\n"
+  echo "Enabling $SERVICE"
+  sudo systemctl enable $SERVICE
 
 done
 
