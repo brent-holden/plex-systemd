ffc3930a4dac09c96e0752fcdd183256af8e8de2
Brent Holden <brent.holden@gmail.com>
2021-02-03 11:07:03 -0500

    Added cache dir creation

--- scripts/setup_rclone.sh
+++ scripts/setup_rclone.sh
@@ -11,29 +11,31 @@ echo -e "You can follow the documentation posted at http://rclone.org\n\n"
 read -p "Point me to rclone.conf (default value: /root/.config/rclone/rclone.conf): " RCLONECONF
 RCLONECONF=${RCLONECONF:-/root/.config/rclone/rclone.conf}
 
-if [ ! -f "$RCLONECONF" ]; then
+if [ ! -f "${RCLONECONF}" ]; then
   echo "File not found. Exiting"
   exit 1
 fi
 
 echo "Making service directories"
-sudo mkdir -p $DOWNLOADSDIR
-sudo mkdir -p $RCLONEMEDIADIR
-sudo mkdir -p $RCLONEBACKUPDIR
-sudo mkdir -p $RCLONECONFIGDIR
+sudo mkdir -p ${DOWNLOADSDIR}
+sudo mkdir -p ${RCLONEMEDIADIR}
+sudo mkdir -p ${RCLONECACHEDIR}
+sudo mkdir -p ${RCLONECACHEDBDIR}
+sudo mkdir -p ${RCLONEBACKUPDIR}
+sudo mkdir -p ${RCLONECONFIGDIR}
 
-sudo cp $RCLONECONF $RCLONECONFIGDIR
+sudo cp ${RCLONECONF} ${RCLONECONFIGDIR}
 
 echo "Installing rclone"
 sudo yum install -y https://downloads.rclone.org/rclone-current-linux-amd64.rpm
 
 echo "Copying service files over"
-sudo cp $SYSTEMDSVCFILESDIR/rclone* $SYSTEMDDIR
+sudo cp ${SYSTEMDSVCFILESDIR}/rclone* ${SYSTEMDDIR}
 sudo systemctl daemon-reload
 
 echo "Enabling and starting rclone services"
 sudo systemctl enable --now rclone-media-drive
 sudo systemctl enable --now rclone-backup-drive
-sudo systemctl enable --now rclone-web
+#sudo systemctl enable --now rclone-web
 
 echo "rClone Installed!"
